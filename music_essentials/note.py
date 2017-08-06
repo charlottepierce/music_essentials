64da375ee715cdb1715f5850f3a9028aaafbcd1a
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-07 08:39:30 +1000

    Add validation to Note class.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- music_essentials/note.py
+++ music_essentials/note.py
@@ -1,7 +1,30 @@
 class Note(object):
+    VALID_PITCHES = ('C', 'D', 'E', 'F', 'G', 'A', 'B')
+    VALID_ACCIDENTALS = ('#', '##', 'b', 'bb')
+
     def __init__(self, pitch, octave, accidental=None):
+        if not isinstance(pitch, str):
+            raise ValueError('Expected string for pitch - (provided ' + str(pitch) + ')')
+        if pitch.upper() not in Note.VALID_PITCHES:
+            raise ValueError('Invalid pitch: ' + str(pitch))
+
+        try:
+            int(octave) # test if octave value is a number
+        except:
+            raise ValueError('Expected integer for octave - (provided ' + str(octave) + ')')
+        if '.' in str(octave): # check that the number doesn't have a decimal place
+            raise ValueError('Expected integer for octave - (provided ' + str(octave) + ')')
+        if (int(octave) < -1) or (int(octave) > 9):
+            raise ValueError('Octave needs to be in the range [-1, 9] - (provided ' + str(octave) + ')')
+
+        if accidental is not None:
+            if not isinstance(accidental, str):
+                raise ValueError('Expected string for accidental - provided ' + str(accidental) + ')')
+            if accidental.lower() not in Note.VALID_ACCIDENTALS:
+                raise ValueError('Invalid accidental: ' + str(accidental))
+
         self.pitch = pitch.upper()
-        self.octave = octave
+        self.octave = int(octave)
         self.accidental = accidental
 
         if accidental is not None:
