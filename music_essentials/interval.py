826cb81f06141785a2c62c5db0334dd7d5178de1
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-08 13:19:14 +1000

    Add docstring to Interval constructor, change 'distance' field to 'size'.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- music_essentials/interval.py
+++ music_essentials/interval.py
@@ -4,28 +4,56 @@ class Interval(object):
     NAMED_INTERVALS = ('M', 'm', 'dim', 'aug')
     """Explicit interval types supported - major, minor, diminished, augmented."""
 
-    def __init__(self, interval_type, distance):
+    def __init__(self, interval_type, size):
+        """Create a new Interval.
+
+        Args:
+            interval_type : str
+                The type of interval. Should be one of:attr:`~music_essentials.interval.Interval.NAMED_INTERVALS`.
+            distance : int
+                The size of the interval. Should be positive.
+
+        Returns:
+            Interval
+                A new interval of the given type and size..
+
+        Raises:
+            ValueError
+                If an invalid interval type or size is provided.
+        
+        Examples:
+            >>> i = Interval('M', 4)
+            >>> print(i)
+            M4
+            >>> i = Interval('dim', 13)
+            >>> print(i)
+            dim13
+            >>> i = Interval('i', 6)
+            ValueError: Unsupported interval type specified: i
+            >>> i = Interval('m', -1)
+            ValueError: Expected interval distance to be positive (provided -1)
+        """
         if interval_type not in Interval.NAMED_INTERVALS:
             raise ValueError('Unsupported interval type specified: ' + str(interval_type))
 
         try:
-            int(distance) # test if distance value is a number
+            int(size) # test if distance value is a number
         except:
-            raise ValueError('Expected integer for interval distance (provided ' + str(distance) + ')')
-        if '.' in str(distance): # check that the distance number doesn't have a decimal place
-            raise ValueError('Expected integer for interval distance (provided ' + str(distance) + ')')
-        if int(distance) < 0:
-            raise ValueError('Expected interval distance to be positive (provided ' + str(distance) + ')')
+            raise ValueError('Expected integer for interval distance (provided ' + str(size) + ')')
+        if '.' in str(size): # check that the distance number doesn't have a decimal place
+            raise ValueError('Expected integer for interval distance (provided ' + str(size) + ')')
+        if int(size) < 0:
+            raise ValueError('Expected interval distance to be positive (provided ' + str(size) + ')')
 
         self.interval_type = interval_type
-        self.distance = int(distance)
+        self.size = int(size)
     
     @classmethod
     def from_interval_string(cls, interval_string):
-        distance = interval_string[-1]
+        size = interval_string[-1]
         interval_type = interval_string[:-1]
 
-        return cls(interval_type, distance)
+        return cls(interval_type, size)
 
     def __str__(self):
         return self.interval_type + str(self.distance)
--- tests/test_interval.py
+++ tests/test_interval.py
@@ -19,13 +19,17 @@ def test_manual_interval_creation_correct_interval_augmented():
     i = Interval('aug', 5)
     assert i.interval_type == 'aug'
 
-def test_manual_interval_creation_correct_distance_whole_number():
+def test_manual_interval_creation_correct_size_whole_number():
     i = Interval('aug', 5)
-    assert i.distance == 5
+    assert i.size == 5
 
-def test_manual_interval_creation_correct_distance_as_string():
+def test_manual_interval_creation_correct_size_two_digit():
+    i = Interval('aug', 15)
+    assert i.size == 15
+
+def test_manual_interval_creation_correct_size_as_string():
     i = Interval('aug', '7')
-    assert i.distance == 7
+    assert i.size == 7
 
 # Interval creation from string - correct values
 def test_interval_creation_correct_interval_major():
@@ -44,24 +48,28 @@ def test_interval_creation_correct_interval_augmented():
     i = Interval.from_interval_string('aug5')
     assert i.interval_type == 'aug'
 
-def test_interval_creation_correct_distance():
+def test_interval_creation_correct_size_two_digit():
+    i = Interval('aug15')
+    assert i.size == 15
+
+def test_interval_creation_correct_size():
     i = Interval.from_interval_string('aug5')
-    assert i.distance == 5
+    assert i.size == 5
 
 # Manual interval creation - incorrect values
 def test_manual_interval_creation_incorrect_interval_type():
     with pytest.raises(ValueError):
         i = Interval('asd', 5)
 
-def test_manual_interval_creation_incorrect_distance_negative():
+def test_manual_interval_creation_incorrect_size_negative():
     with pytest.raises(ValueError):
         i = Interval('M', -5)
 
-def test_manual_interval_creation_incorrect_distance_float_value():
+def test_manual_interval_creation_incorrect_size_float_value():
     with pytest.raises(ValueError):
         i = Interval('M', 5.5)
 
-def test_manual_interval_creation_incorrect_distance_string():
+def test_manual_interval_creation_incorrect_size_string():
     with pytest.raises(ValueError):
         i = Interval('M', 'butts')
 
@@ -70,14 +78,14 @@ def test_interval_creation_incorrect_interval_type():
     with pytest.raises(ValueError):
         i = Interval.from_interval_string('asd5')
 
-def test_interval_creation_incorrect_distance_negative():
+def test_interval_creation_incorrect_size_negative():
     with pytest.raises(ValueError):
         i = Interval.from_interval_string('M-5')
 
-def test_interval_creation_incorrect_distance_float_value():
+def test_interval_creation_incorrect_size_float_value():
     with pytest.raises(ValueError):
         i = Interval.from_interval_string('M5.5')
 
-def test_interval_creation_incorrect_distance_string():
+def test_interval_creation_incorrect_size_string():
     with pytest.raises(ValueError):
         i = Interval.from_interval_string('Mbutts')
