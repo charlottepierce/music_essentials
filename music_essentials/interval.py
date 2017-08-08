fb03f832b7212803abbbf6983f44c6e42f37defa
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-08 13:13:18 +1000

    Add validation to Interval constructors.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- music_essentials/interval.py
+++ music_essentials/interval.py
@@ -1,7 +1,22 @@
 class Interval(object):
     """Representation of an interval (i.e., gap) between notes."""
 
+    NAMED_INTERVALS = ('M', 'm', 'dim', 'aug')
+    """Explicit interval types supported - major, minor, diminished, augmented."""
+
     def __init__(self, interval_type, distance):
+        if interval_type not in Interval.NAMED_INTERVALS:
+            raise ValueError('Unsupported interval type specified: ' + str(interval_type))
+
+        try:
+            int(distance) # test if distance value is a number
+        except:
+            raise ValueError('Expected integer for interval distance (provided ' + str(distance) + ')')
+        if '.' in str(distance): # check that the distance number doesn't have a decimal place
+            raise ValueError('Expected integer for interval distance (provided ' + str(distance) + ')')
+        if int(distance) < 0:
+            raise ValueError('Expected interval distance to be positive (provided ' + str(distance) + ')')
+
         self.interval_type = interval_type
         self.distance = int(distance)
     
