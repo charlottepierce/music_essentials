aa302ced782d528076bcea8add0ddf4be1d428e2
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-16 14:05:00 +1000

    Change scale pattern shortcuts to private and add string keys.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- music_essentials/scale.py
+++ music_essentials/scale.py
@@ -5,15 +5,23 @@
 from .interval import Interval, _TONE, _SEMITONE, _TONE_AND_HALF
 
 class Scale(object):
-    MAJOR = (_TONE, _TONE, _SEMITONE, _TONE, _TONE, _TONE, _SEMITONE)
-    HARMONIC_MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE_AND_HALF, _SEMITONE)
+    _MAJOR = (_TONE, _TONE, _SEMITONE, _TONE, _TONE, _TONE, _SEMITONE)
+    _HARMONIC_MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE_AND_HALF, _SEMITONE)
+
+    _SCALE_PATTERNS = {
+        'major': _MAJOR,
+        'maj': _MAJOR,
+        'minor': _HARMONIC_MINOR,
+        'min': _HARMONIC_MINOR
+    }
 
     @classmethod
     def build_scale(cls, tonic, scale_type):
         # TODO: docstring (does support building of theoretical scales, as long as the result doesn't contain any accidentals above double sharps/flats)
         # TODO: validation (and tests for validation)
+        scale_pattern = Scale._SCALE_PATTERNS[scale_type]
         scale = [tonic]
-        for diff in scale_type:
+        for diff in scale_pattern:
             new = scale[-1] + diff
             scale.append(new)
 
