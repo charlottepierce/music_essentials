cc3c556591e0fcfc279c2d1768738abc78ad68bc
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-16 13:25:25 +1000

    Rename MINOR to HARMONIC_MINOR.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- music_essentials/scale.py
+++ music_essentials/scale.py
@@ -2,7 +2,7 @@ from .interval import Interval, _TONE, _SEMITONE, _TONE_AND_HALF
 
 class Scale(object):
     MAJOR = (_TONE, _TONE, _SEMITONE, _TONE, _TONE, _TONE, _SEMITONE)
-    MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE_AND_HALF, _SEMITONE)
+    HARMONIC_MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE_AND_HALF, _SEMITONE)
 
     @classmethod
     def build_scale(cls, tonic, scale_type):
