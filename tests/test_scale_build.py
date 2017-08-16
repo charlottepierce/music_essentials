b89219eefde40464744431eddaee7ac89e4d2eaf
Charlotte Pierce <charlotte@malformed-bits.com>
2017-08-17 08:22:01 +1000

    Add tests for building harmonic minor scales.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- tests/test_scale_build.py
+++ tests/test_scale_build.py
@@ -298,4 +298,298 @@ def test_scale_builder_c_flat_maj():
     assert s == expected
 
 # Test building of harmonic minor scales
+def test_scale_builder_c_min():
+    s = Scale.build_scale(Note('C', 4), 'minor')
+    expected = [
+        Note('C', 4),
+        Note('D', 4),
+        Note('E', 4, 'b'),
+        Note('F', 4),
+        Note('G', 4),
+        Note('A', 4, 'b'),
+        Note('B', 4),
+        Note('C', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_c_sharp_min():
+    s = Scale.build_scale(Note('C', 4, '#'), 'min')
+    expected = [
+        Note('C', 4, '#'),
+        Note('D', 4, '#'),
+        Note('E', 4),
+        Note('F', 4, '#'),
+        Note('G', 4, '#'),
+        Note('A', 4),
+        Note('B', 4, '#'),
+        Note('C', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_d_flat_min():
+    s = Scale.build_scale(Note('D', 4, 'b'), 'minor')
+    expected = [
+        Note('D', 4, 'b'),
+        Note('E', 4, 'b'),
+        Note('F', 4, 'b'),
+        Note('G', 4, 'b'),
+        Note('A', 4, 'b'),
+        Note('B', 4, 'bb'),
+        Note('C', 5),
+        Note('D', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_d_min():
+    s = Scale.build_scale(Note('D', 4), 'min')
+    expected = [
+        Note('D', 4),
+        Note('E', 4),
+        Note('F', 4),
+        Note('G', 4),
+        Note('A', 4),
+        Note('B', 4, 'b'),
+        Note('C', 5, '#'),
+        Note('D', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_d_sharp_min():
+    s = Scale.build_scale(Note('D', 4, '#'), 'minor')
+    expected = [
+        Note('D', 4, '#'),
+        Note('E', 4, '#'),
+        Note('F', 4, '#'),
+        Note('G', 4, '#'),
+        Note('A', 4, '#'),
+        Note('B', 4),
+        Note('C', 5, '##'),
+        Note('D', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_e_flat_min():
+    s = Scale.build_scale(Note('E', 4, 'b'), 'min')
+    expected = [
+        Note('E', 4, 'b'),
+        Note('F', 4),
+        Note('G', 4, 'b'),
+        Note('A', 4, 'b'),
+        Note('B', 4, 'b'),
+        Note('C', 5, 'b'),
+        Note('D', 5),
+        Note('E', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_e_min():
+    s = Scale.build_scale(Note('E', 4), 'minor')
+    expected = [
+        Note('E', 4),
+        Note('F', 4, '#'),
+        Note('G', 4),
+        Note('A', 4),
+        Note('B', 4),
+        Note('C', 5),
+        Note('D', 5, '#'),
+        Note('E', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_e_sharp_min():
+    s = Scale.build_scale(Note('E', 4, '#'), 'min')
+    expected = [
+        Note('E', 4, '#'),
+        Note('F', 4, '##'),
+        Note('G', 4, '#'),
+        Note('A', 4, '#'),
+        Note('B', 4, '#'),
+        Note('C', 5, '#'),
+        Note('D', 5, '##'),
+        Note('E', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_f_flat_min():
+    s = Scale.build_scale(Note('F', 4, 'b'), 'minor')
+    expected = [
+        Note('F', 4, 'b'),
+        Note('G', 4, 'b'),
+        Note('A', 4, 'bb'),
+        Note('B', 4, 'bb'),
+        Note('C', 5, 'b'),
+        Note('D', 5, 'bb'),
+        Note('E', 5, 'b'),
+        Note('F', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_f_min():
+    s = Scale.build_scale(Note('F', 4), 'min')
+    expected = [
+        Note('F', 4),
+        Note('G', 4),
+        Note('A', 4, 'b'),
+        Note('B', 4, 'b'),
+        Note('C', 5),
+        Note('D', 5, 'b'),
+        Note('E', 5),
+        Note('F', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_f_sharp_min():
+    s = Scale.build_scale(Note('F', 4, '#'), 'minor')
+    expected = [
+        Note('F', 4, '#'),
+        Note('G', 4, '#'),
+        Note('A', 4),
+        Note('B', 4),
+        Note('C', 5, '#'),
+        Note('D', 5),
+        Note('E', 5, '#'),
+        Note('F', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_g_flat_min():
+    s = Scale.build_scale(Note('G', 4, 'b'), 'min')
+    expected = [
+        Note('G', 4, 'b'),
+        Note('A', 4, 'b'),
+        Note('B', 4, 'bb'),
+        Note('C', 5, 'b'),
+        Note('D', 5, 'b'),
+        Note('E', 5, 'bb'),
+        Note('F', 5),
+        Note('G', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_g_min():
+    s = Scale.build_scale(Note('G', 4), 'minor')
+    expected = [
+        Note('G', 4),
+        Note('A', 4),
+        Note('B', 4, 'b'),
+        Note('C', 5),
+        Note('D', 5),
+        Note('E', 5, 'b'),
+        Note('F', 5, '#'),
+        Note('G', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_g_sharp_min():
+    s = Scale.build_scale(Note('G', 4, '#'), 'min')
+    expected = [
+        Note('G', 4, '#'),
+        Note('A', 4, '#'),
+        Note('B', 4),
+        Note('C', 5, '#'),
+        Note('D', 5, '#'),
+        Note('E', 5),
+        Note('F', 5, '##'),
+        Note('G', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_a_flat_min():
+    s = Scale.build_scale(Note('A', 4, 'b'), 'minor')
+    expected = [
+        Note('A', 4, 'b'),
+        Note('B', 4, 'b'),
+        Note('C', 5, 'b'),
+        Note('D', 5, 'b'),
+        Note('E', 5, 'b'),
+        Note('F', 5, 'b'),
+        Note('G', 5),
+        Note('A', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_a_min():
+    s = Scale.build_scale(Note('A', 4), 'min')
+    expected = [
+        Note('A', 4),
+        Note('B', 4),
+        Note('C', 5),
+        Note('D', 5),
+        Note('E', 5),
+        Note('F', 5),
+        Note('G', 5, '#'),
+        Note('A', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_a_sharp_min():
+    s = Scale.build_scale(Note('A', 4, '#'), 'minor')
+    expected = [
+        Note('A', 4, '#'),
+        Note('B', 4, '#'),
+        Note('C', 5, '#'),
+        Note('D', 5, '#'),
+        Note('E', 5, '#'),
+        Note('F', 5, '#'),
+        Note('G', 5, '##'),
+        Note('A', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_b_flat_min():
+    s = Scale.build_scale(Note('B', 4, 'b'), 'min')
+    expected = [
+        Note('B', 4, 'b'),
+        Note('C', 5),
+        Note('D', 5, 'b'),
+        Note('E', 5, 'b'),
+        Note('F', 5),
+        Note('G', 5, 'b'),
+        Note('A', 5),
+        Note('B', 5, 'b')
+    ]
+    assert s == expected
+
+def test_scale_builder_b_min():
+    s = Scale.build_scale(Note('B', 4), 'minor')
+    expected = [
+        Note('B', 4),
+        Note('C', 5, '#'),
+        Note('D', 5),
+        Note('E', 5),
+        Note('F', 5, '#'),
+        Note('G', 5),
+        Note('A', 5, '#'),
+        Note('B', 5)
+    ]
+    assert s == expected
+
+def test_scale_builder_b_sharp_min():
+    s = Scale.build_scale(Note('B', 4, '#'), 'min')
+    expected = [
+        Note('B', 4, '#'),
+        Note('C', 5, '##'),
+        Note('D', 5, '#'),
+        Note('E', 5, '#'),
+        Note('F', 5, '##'),
+        Note('G', 5, '#'),
+        Note('A', 5, '##'),
+        Note('B', 5, '#')
+    ]
+    assert s == expected
+
+def test_scale_builder_c_flat_min():
+    s = Scale.build_scale(Note('C', 5, 'b'), 'minor')
+    expected = [
+        Note('C', 5, 'b'),
+        Note('D', 5, 'b'),
+        Note('E', 5, 'bb'),
+        Note('F', 5, 'b'),
+        Note('G', 5, 'b'),
+        Note('A', 5, 'bb'),
+        Note('B', 5, 'b'),
+        Note('C', 6, 'b')
+    ]
+    assert s == expected
+
 # Test building of natural minor scales
