862da92fc5eaf1f2e5e455c55f05b20cb4c4c3dd
Charlotte Pierce <charlotte@malformed-bits.com>
2017-10-24 11:54:47 +1100

    Add setup.py so package can be installed locally.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='music_essentials',
+    version='0.1',
+    description='Music operations for python - create and manipulate scales, chords, and intervals.',
+    url='https://github.com/charlottepierce/music_essentials',
+    author='Charlotte Pierce',
+    author_email='charlotte@malformed-bits.com',
+    license='MIT',
+    packages=['music_essentials']
+)
