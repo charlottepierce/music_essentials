dd329e8397b3b155544c2e26d41b5ce429a652f2
Simon Wood <simon@mungewell.org>
2018-06-19 11:52:01 -0600

    Fix 'tripple sharps' issue when building scales and add more types of scales.
    Current code errors when rendering the Pentatonic scales as the number of
    sharps builds up/exceeds permitted amount. I believe that this is because
    the code progressively builds the scale from the last note used.
    
    This patches code so that it builds using 'root + offset' for each note
    in the scale. Tripple sharp issue no longer occurs.

--- music_essentials/scale.py
+++ music_essentials/scale.py
@@ -2,21 +2,37 @@
 # TODO: specify direction
 
 from .note import Note
-from .interval import Interval, _TONE, _SEMITONE, _TONE_AND_HALF
+from .interval import Interval
 
 class Scale(object):
     """Static class methods for building lists of notes according to pre-defined patterns."""
-    _MAJOR = (_TONE, _TONE, _SEMITONE, _TONE, _TONE, _TONE, _SEMITONE)
-    _HARMONIC_MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE_AND_HALF, _SEMITONE)
-    _NATURAL_MINOR = (_TONE, _SEMITONE, _TONE, _TONE, _SEMITONE, _TONE, _TONE)
+    _MAJOR            = ('M2', 'M3', 'P4', 'P5', 'M6', 'M7', 'P8')
+    _HARMONIC_MINOR   = ('M2', 'm3', 'P4', 'P5', 'm6', 'M7', 'P8')
+    _NATURAL_MINOR    = ('M2', 'm3', 'P4', 'P5', 'm6', 'm7', 'P8')
+    _MELODIC_MINOR    = ('M2', 'm3', 'P4', 'P5', 'M6', 'M7', 'P8')
+    _DORIAN           = ('M2', 'm3', 'P4', 'P5', 'M6', 'm7', 'P8')
+    _LOCRIAN          = ('m2', 'm3', 'P4', 'dim5', 'm6', 'm7', 'P8')
+    _LYDIAN           = ('M2', 'M3', 'aug4', 'P5', 'M6', 'M7', 'P8')
+    _MIXOLYDIAN       = ('M2', 'M3', 'P4', 'P5', 'M6', 'm7', 'P8')
+    _PHRYGIAN         = ('m2', 'm3', 'P4', 'P5', 'm6', 'm7', 'P8')
+    _MAJOR_PENTATONIC = ('M2', 'M3', 'P5', 'M6', 'P8')
+    _MINOR_PENTATONIC = ('m3', 'P4', 'P5', 'm7', 'P8')
 
     _SCALE_PATTERNS = {
-        'major': _MAJOR,
-        'maj': _MAJOR,
-        'minor': _HARMONIC_MINOR,
-        'min': _HARMONIC_MINOR,
-        'natural minor': _NATURAL_MINOR,
-        'nat min' : _NATURAL_MINOR
+        'major'            : _MAJOR,
+        'maj'              : _MAJOR,
+        'minor'            : _HARMONIC_MINOR,
+        'min'              : _HARMONIC_MINOR,
+        'natural minor'    : _NATURAL_MINOR,
+        'nat min'          : _NATURAL_MINOR,
+        'melodic minor'    : _MELODIC_MINOR,
+        'dorian'           : _DORIAN,
+        'locrian'          : _LOCRIAN,
+        'lydian'           : _LYDIAN,
+        'mixolydian'       : _MIXOLYDIAN,
+        'phrygian'         : _PHRYGIAN,
+        'major pentatonic' : _MAJOR_PENTATONIC,
+        'minor pentatonic' : _MINOR_PENTATONIC,
     }
 
     @classmethod
@@ -59,7 +75,7 @@ class Scale(object):
         scale_pattern = Scale._SCALE_PATTERNS[scale_type]
         scale = [tonic]
         for diff in scale_pattern:
-            new = scale[-1] + diff
+            new = scale[0] + Interval.from_interval_string(diff)
             scale.append(new)
 
         return scale
